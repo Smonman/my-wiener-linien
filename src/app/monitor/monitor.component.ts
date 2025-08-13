29854db1ab0ece3e2bedc28b47d1a2e52cdd8ed6
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2025-08-13 20:48:04 +0200

    feat(ui): add line colors

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -3,6 +3,7 @@ import { TitleCasePipe, UpperCasePipe } from "@angular/common";
 import { MonitorService } from "../services/monitor.service";
 import { Monitor } from "../../models/monitor";
 import { MonitorDescriptor } from "../../models/monitor-descriptor";
+import { LineType } from "../../models/line";
 
 @Component({
   selector: "app-monitor",
@@ -22,6 +23,51 @@ export class MonitorComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.monitorService.deregister(this.monitorDescriptor());
+  }
+
+  protected monitorClasses(): string[] {
+    const classes: string[] = [];
+    if (this.monitorSignal && this.monitorSignal()) {
+      switch (this.monitorSignal()?.line.type) {
+        case LineType.METRO:
+          classes.push(this.getUndergroundColor(this.monitorSignal()?.line.name));
+          break;
+        case LineType.TRAM:
+          classes.push("bg-bim");
+          break;
+        case LineType.WLB:
+          classes.push("bg-wlb");
+          break;
+        case LineType.BUS:
+          classes.push("bg-bus");
+          break;
+        case LineType.NIGHT_LINE:
+          classes.push("bg-bus");
+          break;
+        case LineType.S_LINE:
+          classes.push("bg-sb");
+          break;
+      }
+    }
+    return classes;
+  }
+
+  protected getUndergroundColor(lineName: string | undefined | null): string {
+    switch (lineName?.toUpperCase()) {
+      case "U1":
+        return "bg-u1";
+      case "U2":
+        return "bg-u2";
+      case "U3":
+        return "bg-u3";
+      case "U4":
+        return "bg-u4";
+      case "U5":
+        return "bg-u5";
+      case "U6":
+        return "bg-u6";
+    }
+    return ""
   }
 
   protected getCountdown(nextArrivalTime: Date): number {
